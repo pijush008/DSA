d3a174c1c144ab8407b837bfe750ac8052cab26b
Pijush Patra <158040427+pijush008@users.noreply.github.com>
2026-03-07 11:48:31 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (5.27%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        
-int n=matrix.size();
+
+int n= matrix.size();
 int m=matrix[0].size();
 
 vector<int>row(n,0);
 vector<int>col(m,0);
+
 
 for(int i=0;i<n;i++){
     for(int j=0;j<m;j++){
@@ -18,14 +19,16 @@ for(int i=0;i<n;i++){
 }
 
 
+
+
 for(int i=0;i<n;i++){
     for(int j=0;j<m;j++){
-       if(row[i]==1 || col[j]==1){
-        matrix[i][j]=0;
-       }
+        if(row[i]==1 || col[j]==1){
+matrix[i][j]=0;
+        }
     }
 }
 
-        
+
     }
 };
