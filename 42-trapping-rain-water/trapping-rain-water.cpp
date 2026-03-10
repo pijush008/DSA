12d16a6a04169d3f0c29e8c78f5db28e5da73732
Pijush Patra <158040427+pijush008@users.noreply.github.com>
2026-03-10 23:03:03 +0530

    Time: 2 ms (20.36%) | Memory: 28.4 MB (5.05%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        int lm[n];
-        int rm[n];
-        lm[0] = height[0];
-        rm[n - 1] = height[n-1];
-        int ans = 0;
-        for (int i = 1; i < n; i++) {
-            lm[i]=max(lm[i-1],height[i]);
-        }
-        for (int i = n - 2; i >= 0; i--) {
-           rm[i]=max(rm[i+1],height[i]);
-        }
-        
-        for(int i=0;i<n;i++){
-            ans=ans+min(lm[i],rm[i])-height[i];
-        }
-        return ans;
+       
+int n= height.size();
+vector<int>lmax(n);
+vector<int>rmax(n);
+
+vector<int>m(n);
+vector<int>ans(n);
+
+int maxl=INT_MIN;
+for(int i=0;i<n;i++){
+maxl=max(maxl,height[i]);
+lmax[i]=maxl;
+}
+
+int maxr=INT_MIN;
+for(int i=n-1;i>=0;i--){
+maxr=max(maxr,height[i]);
+rmax[i]=maxr;
+}
+
+for(int i=0;i<n;i++){
+    m[i]=min(rmax[i],lmax[i]);
+}
+
+for(int i=0;i<n;i++){
+   ans[i]=m[i]-height[i];
+}
+
+long long sum=0;
+
+for(int i=0;i<n;i++){
+    sum+=ans[i];
+}
+
+return sum;
+
     }
 };
